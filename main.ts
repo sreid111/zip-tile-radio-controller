0ee6a1c0a3154aad3b4aa7a8ad140a14179553ca
Mr Reid <64812253+sreid111@users.noreply.github.com>
2022-09-30 15:38:40 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendNumber(0)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendNumber(1)
+})
+radio.setGroup(11)
+basic.showString("light")
